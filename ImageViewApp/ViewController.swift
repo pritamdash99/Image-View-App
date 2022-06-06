7e55d9ea99a11e9ea2b4dd7a181189f34a03fa9f
Pritam Dash <pritamdash2015@outlook.com>
2022-06-06 22:17:35 +0530

    Image view and Segmented Control behaving nicely.

--- ImageViewApp/ViewController.swift
+++ ImageViewApp/ViewController.swift
@@ -9,9 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var imageView : UIImageView?
+    @IBOutlet var segmentedControl : UISegmentedControl?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func segmentedControlSelected() -> Void {
+        switch segmentedControl!.selectedSegmentIndex {
+        case 0 :
+            let image = UIImage(named: "natureImage1.jpg")
+            imageView?.image = image
+        case 1 :
+            let image = UIImage(named: "natureImage2.jpg")
+            imageView?.image = image
+        case 2 :
+            let image = UIImage(named: "natureImage3.jpg")
+            imageView?.image = image
+        default :
+            print("Out of bounds selection")
+        }
     }
 
 
